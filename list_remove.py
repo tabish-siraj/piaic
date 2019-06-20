5aa5dadb0d89e80c622b5c74e00b5404d981ad54
Tabish1226 <Tabish1226@users.noreply.github.com>
2019-06-19 23:28:15 -0700

    Update list_remove.py

--- list_remove.py
+++ list_remove.py
@@ -1,10 +1,13 @@
-#creating a list
+#creating a list and printing
 lst = [2, 5, 3, 5, 6, 5, 8, 5, 9, 5]
 print("Initial List:\n",lst)
 
+#Taking number which is to remove from the list
+num = int(input("Input the number you want to remove:\t"))
 #removing all the same elements using loop
 for x in lst:
-    lst.remove(5)
+    if x == num:
+        lst.remove(num)
 
 #final elements
 print("Final List:\n",lst)
